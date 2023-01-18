de680f280d47cad0a3abaf6692279798455fa535
Yesi78 <97620920+Yesi78@users.noreply.github.com>
2023-01-18 14:40:24 -0500

    Delete Dealer_and_Loss.sh

--- Dealer_and_Loss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-grep '05:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/*_schedule | grep AM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
-grep '08:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/*_schedule | grep AM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
-grep '02:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/*_schedule | grep PM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
-grep '08:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/0310_Dealer_schedule | grep PM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
-grep '08:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/0312_Dealer_schedule | grep PM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
-grep '11:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/0310_Dealer_schedule | grep PM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
-grep '11:00*' ~/Lucky_Duck_Investigations/Roulette_Loss_Investigation/Dealer_Analysis/0312_Dealer_schedule | grep PM | awk -F" " '{print $5,$6}' >> Dealer_working_during_losses
